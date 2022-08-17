531daae534d80a856a805a90ca7db858f8f0590b
Kiril Savino <kiril.savino@gmail.com>
2022-08-16 20:49:39 -0400

    spiffed up license

--- js/license.mjs
+++ js/license.mjs
@@ -6,6 +6,8 @@ import fs from 'fs'
 import path from 'path'
 import ora from 'ora'
 import spinners from 'cli-spinners'
+import QRCode from 'qrcode'
+import web3 from '@solana/web3.js'
 
 const ASSETS_DIR = './assets/NFT2'
 const QR_IMAGE = './assets/NFT2/0.png'
@@ -31,12 +33,19 @@ const exec = async (command) => {
     })
 }
 
+const getMintId = async (txId) => {
+    const solana = new web3.Connection("https://yolo-cool-silence.solana-devnet.discover.quiknode.pro/8a2c88416f21d62a416c6a237638f530d45b6a84/");
+    //const txId = "26Wpe4UQgyvsP1t5qG8LS6DEnyjcWF2mM4rtPqH4ufndEvpNPRafCd6okvQv2Kk1mw8xryE6Pk1R66NryLsjKw17"
+    const info = await solana.getTransaction(txId)
+    const { meta: { postTokenBalances } } = info
+    return postTokenBalances[0].mint
+}
+
 const getVerifyUrl = (userId) => {
     const encode = (str) => encodeURIComponent(str).replace(/[!'()*]/g, (c) => `%${c.charCodeAt(0).toString(16).toUpperCase()}`)
     const query = `{getHistoryById(id: "${userId}") { KanyeSpotify FollowsKanye JeenYuhs userId } }`
     return 'https://saopedro.stepzen.net/api/orange-bee/__graphql?query=' + encode(query);
 }
-
 
 const generateAssetJSON = (verificationUrl) => {
     const manifest = {
@@ -65,11 +74,6 @@ const generateAssetJSON = (verificationUrl) => {
     fs.writeFileSync(jsonPath, JSON.stringify(manifest, null, 2))
 }
 
-//  const generateAssetImage = async (verificationUrl) => {
-//      await generateQRCode(verificationUrl)
-//      const imagePath = path.join(ASSETS_DIR, '1.png')
-// }
-
    const generateAssets = async (verificationUrl) => {
        generateAssetJSON(verificationUrl)
        //await generateAssetImage(verificationUrl, mintId)
@@ -91,17 +95,13 @@ const isTrueYeezy = async (userId) => {
 
 const run = async (opts) => {
     const { id: userId } = opts
-    console.log('Assembling Assets for License NFT...')
+    console.log('Creating License NFT')
     const qualifies = await isTrueYeezy(userId)
     if (!qualifies) {
         console.log('Ooooh, so sorry, no NFT for you')
         return
     }
-    console.log('--=| User qualifies! |=--')
     const verificationUrl = getVerifyUrl(userId)
-
-    console.log('Generating Assets...')
-    generateAssets(verificationUrl)
 
     const spinWhile = async (promise, text) => {
         const spinner = ora({ text, spinner: spinners.point, color: 'red' }).start()
@@ -109,20 +109,23 @@ const run = async (opts) => {
         spinner.succeed()
     }
 
+    await spinWhile(generateAssets(verificationUrl), 'Assembling Assets for License NFT', 'Assembled Assets for License NFT')
+
     await spinWhile(exec(`${VERIFY_ASSETS_COMMAND} ${ASSETS_DIR}`), 'Verify assets (1/5)')
     await spinWhile(exec(`${UPLOAD_COMMAND} ${ASSETS_DIR}`), 'Upload assets (2/5)')
     await spinWhile(exec(COLLECT_COMMAND), 'Set collection (3/5)')
     await spinWhile(exec(VERIFY_UPLOAD_COMMAND), 'Verify upload (4/5)')
-    let nftKey = null
+    let txId = null
     const mint = async () => {
         const output = await exec(MINT_COMMAND)
         //console.log(output)
         const regex = /mint_one_token finished ([A-Za-z0-9]+)/g
-        nftKey = regex.exec(output)[1]
+        txId = regex.exec(output)[1]
     }
-    await spinWhile(mint(), 'Minting License NFT (4/5)', 'Minted License (4/5)')
-    console.log('NFT Key', nftKey)
-    //exec(`open https://explorer.solana.com/address/${mintId}?cluster=devnet`)
+    await spinWhile(mint(), 'Mint License NFT (5/5)')
+    console.log('Transaction ID', txId)
+    const mintId = await getMintId(txId)
+    console.log('Mint Key', mintId)
 }
 
 program
